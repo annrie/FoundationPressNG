02341003fef063740e5dbee0f9ae2daea7e170f6
annrie <annrie@users.noreply.github.com>
2020-09-07 14:25:12 +0900

    config: custom Image Size Adjustment

--- library/responsive-images.php
+++ library/responsive-images.php
@@ -12,33 +12,29 @@
  */
 
 // Add additional image sizes
-add_image_size( 'xsmall', 300 );
-add_image_size( 'small', 640 );
+add_image_size( 'xsmall', 640 );
 add_image_size( 'medium_large', 768 );
-add_image_size( 'medium', 1024 );
-add_image_size( 'large', 1200 );
+add_image_size( 'large_wide', 1200 );
 add_image_size( 'xlarge', 1536 );
 add_image_size( 'xxlarge', 2048 );
-add_image_size( 'full', 2560 );
 
 // Remove medium large srcset image
-add_filter(
-	'intermediate_image_sizes',
-	function ( $sizes ) {
-		return array_diff( $sizes, array( 'medium_large' ) );
-	}
-);
+// add_filter(
+// 'intermediate_image_sizes',
+// function ( $sizes ) {
+// return array_diff( $sizes, array( 'medium_large' ) );
+// }
+// );
 
 // Adjust media library to new sizes
 function ajust_media_library( $response, $attachment, $meta ) {
 	if ( ! empty( $response ) ) {
 		$sizes = array(
-            'small', // WP default
-            'medium', // WP default
-            'large', // WP default
-            'medium_large', // WP default
-            '1536x1535', // WP default
-            '2048x2048', // WP default
+            'xsmall',
+            'medium_large',
+            'large_wide',
+            'xlarge', // WP default
+            'xxlarge', // WP default
         );
     $scount    = count($size) > 0;
 		while ( $scount ) {
@@ -56,23 +52,22 @@ add_filter( 'wp_prepare_attachment_for_js', 'ajust_media_library', 999, 3 );
 // Register the new image sizes for use in the add media modal in wp-admin
 function foundationpress_custom_sizes( $sizes ) {
 	// Unset WP Default sizes so they can be reordered with new custom sizes
-	unset( $sizes['thumbnail'] );
+	// unset( $sizes['thumbnail'] );
 	unset( $sizes['medium'] );
-	unset( $sizes['medium_large'] );
 	unset( $sizes['large'] );
 	unset( $sizes['full'] );
 
 	return array_merge(
 		$sizes,
 		array(
-            'xsmall'  => __( 'XSmall' ),
-            'small'   => __( 'Small' ),
-            'xmedium' => __( 'XMedium' ),
-            'medium'  => __( 'Medium' ),
-            'large'   => __( 'Large' ),
-            'xlarge'  => __( 'XLarge' ),
-            'xxlarge' => __( 'XXLarge'),
-            'full'    => __( 'Full'),
+            'xsmall'       => __( 'Xsmall' ),
+            'medium'       => __( 'Medium' ),
+            'medium_large' => __( 'MediumLarge' ),
+            'large'        => __( 'Large' ),
+            'large_wide'   => __( 'LargeWide' ),
+            'xlarge'       => __( 'XLarge' ),
+            'xxlarge'      => __( 'XXLarge' ),
+            'full'         => __( 'Full' ),
 		)
 	);
 }
