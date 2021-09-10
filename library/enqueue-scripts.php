1eb791f22a4ac3c98a3383cad1b3f290fcada209
annrie <annrie@users.noreply.github.com>
2021-09-10 23:58:41 +0900

    refactor: some modifications by Theme check

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -12,70 +12,73 @@
 // Check to see if rev-manifest exists for CSS and JS static asset revisioning
 // https://github.com/sindresorhus/gulp-rev/blob/master/integration.md
 
-if ( ! function_exists( 'foundationpress_asset_path' ) ) :
-	function foundationpress_asset_path( $filename ) {
-		$filename_split = explode( '.', $filename );
-		$dir            = end( $filename_split );
-		$manifest_path  = dirname( dirname( __FILE__ ) ) . '/dist/assets/' . $dir . '/rev-manifest.json';
+if (! function_exists('foundationpress_asset_path')) :
+    function foundationpress_asset_path($filename)
+    {
+        $filename_split = explode('.', $filename);
+        $dir            = end($filename_split);
+        $manifest_path  = dirname(dirname(__FILE__)) . '/dist/assets/' . $dir . '/rev-manifest.json';
 
-		if ( file_exists( $manifest_path ) ) {
-			$manifest = json_decode( wp_remote_retrieve_body( $manifest_path ), true );
-		} else {
-			$manifest = array();
-		}
+        if (file_exists($manifest_path)) {
+            $manifest = json_decode(wp_remote_retrieve_body($manifest_path), true);
+        } else {
+            $manifest = array();
+        }
 
-		if ( array_key_exists( $filename, array( $manifest ) ) ) {
-			return $manifest[ $filename ];
-		}
-		return $filename;
-	}
+        if (array_key_exists($filename, array( $manifest ))) {
+            return $manifest[ $filename ];
+        }
+        return $filename;
+    }
 endif;
 
-if ( ! function_exists( 'foundationpress_scripts' ) ) :
-	function foundationpress_scripts() {
-    // Enqueue the main Stylesheet.
-		wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/dist/assets/css/' . foundationpress_asset_path( 'app.css' ), array(), filemtime( get_theme_file_path( '/dist/assets/css/app.css' ) ), 'all' );
+if (! function_exists('foundationpress_scripts')) :
+    function foundationpress_scripts()
+    {
+        // Enqueue the main Stylesheet.
+        wp_enqueue_style('main-stylesheet', get_stylesheet_directory_uri() . '/dist/assets/css/' . foundationpress_asset_path('app.css'), array(), filemtime(get_theme_file_path('/dist/assets/css/app.css')), 'all');
 
-		// Deregister the jquery version bundled with WordPress.
-		wp_deregister_script( 'jquery' );
+        // Deregister the jquery version bundled with WordPress.
+        // wp_deregister_script( 'jquery' );
 
-		// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-		wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', false );
+        // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+        // wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', false );
 
-		// Deregister the jquery-migrate version bundled with WordPress.
-		wp_deregister_script( 'jquery-migrate' );
+        // Deregister the jquery-migrate version bundled with WordPress.
+        // wp_deregister_script( 'jquery-migrate' );
 
-		// CDN hosted jQuery migrate for compatibility with jQuery 3.x
-		wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array( 'jquery' ), '3.0.1', false );
+        // CDN hosted jQuery migrate for compatibility with jQuery 3.x
+        // wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array( 'jquery' ), '3.0.1', false );
 
-		// Enqueue jQuery migrate. Uncomment the line below to enable.
-		// wp_enqueue_script( 'jquery-migrate' );
+        // Enqueue jQuery migrate. Uncomment the line below to enable.
+        // wp_enqueue_script( 'jquery-migrate' );
 
-		// Enqueue Foundation scripts
-		wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/dist/assets/js/' . foundationpress_asset_path( 'app.js' ), array( 'jquery' ), '2.10.7', true );
+        // Enqueue Foundation scripts
+        wp_enqueue_script('foundation', get_stylesheet_directory_uri() . '/dist/assets/js/' . foundationpress_asset_path('app.js'), array( 'jquery' ), '2.10.7', true);
 
-		// Enqueue FontAwesome from CDN. Uncomment the line below if you need FontAwesome.
-    wp_enqueue_script( 'fontawesome', '//kit.fontawesome.com/1da9fe040f.js', array(), '6', true );
-		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-			wp_enqueue_script( 'comment-reply' );
+        // Enqueue FontAwesome from CDN. Uncomment the line below if you need FontAwesome.
+        wp_enqueue_script('fontawesome', 'https://kit.fontawesome.com/9d5f8281c5.js', array(), '6', true);
+        if (is_singular() && comments_open() && get_option('thread_comments')) {
+            wp_enqueue_script('comment-reply');
+        }
+
+        add_filter('script_loader_tag', 'add_defer', 10, 2);
+        function add_defer($tag, $handle)
+        {
+            if ('fontawesome' !== $handle) {
+                return $tag;
+            }
+            return str_replace(' src=', ' defer  crossorigin="anonymous" src=', $tag);
+        }
     }
-
-    add_filter('script_loader_tag', 'add_defer', 10, 2);
-    function add_defer( $tag, $handle ) {
-    if ( 'fontawesome' !== $handle ) {
-        return $tag;
-    }
-    return str_replace( ' src=', ' defer  crossorigin="anonymous" src=', $tag );
-    }
-}
-add_action( 'wp_enqueue_scripts', 'foundationpress_scripts' );
+add_action('wp_enqueue_scripts', 'foundationpress_scripts');
 
 /* enqueue_block_editor_assets is only available on the editor screen, and is not loaded on the actual page. If you set it to enqueue_block_assets, it will be loaded in the actual page. */
 add_action(
-'enqueue_block_editor_assets',
-function() {
-    $custom_css_url = '/dist/assets/css/editor.css';
-    wp_enqueue_style( 'my-block', get_stylesheet_directory_uri() . $custom_css_url, array( 'wp-block-library' ), array(), false, true );
-}
+    'enqueue_block_editor_assets',
+    function () {
+        $custom_css_url = '/dist/assets/css/editor.css';
+        wp_enqueue_style('my-block', get_stylesheet_directory_uri() . $custom_css_url, array( 'wp-block-library' ), array(), false, true);
+    }
 );
 endif;
--- library/responsive-images.php
+++ library/responsive-images.php
@@ -12,14 +12,14 @@
  */
 
 // Add additional image sizes
-add_image_size( 'xsmall', 300 );
-add_image_size( 'small', 640 );
-add_image_size( 'medium_large', 768 );
-add_image_size( 'medium', 1024 );
-add_image_size( 'large', 1200 );
-add_image_size( 'xlarge', 1536 );
-add_image_size( 'xxlarge', 2048 );
-add_image_size( 'full', 2560 );
+add_image_size('xsmall', 300);
+add_image_size('small', 640);
+add_image_size('medium_large', 768);
+add_image_size('medium', 1024);
+add_image_size('large', 1200);
+add_image_size('xlarge', 1536);
+add_image_size('xxlarge', 2048);
+add_image_size('full', 2560);
 
 // Remove medium large srcset image
 // add_filter(
@@ -30,83 +30,88 @@ add_image_size( 'full', 2560 );
 // );
 
 // Adjust media library to new sizes
-function ajust_media_library( $response, $attachment, $meta ) {
-	if ( ! empty( $response ) ) {
-		$sizes = array(
+function ajust_media_library($response, $attachment, $meta)
+{
+    if (! empty($response)) {
+        $sizes = array(
             'small', // WP default
             'medium', // WP default
             'large', // WP default
             'medium_large', // WP default
             'xlarge', // WP default
             'xxlarge', // WP default
+            'full',
         );
-    $scount    = count($size) > 0;
-		while ( $scount ) {
-			$cur_size = array_pop( $sizes );
-			if ( isset( $response['sizes'][ $cur_size ] ) ) {
-				$response['sizes']['medium'] = $response['sizes'][ $cur_size ];
+        $scount    = count($sizes) > 0;
+        while ($scount) {
+            $cur_size = array_pop($sizes);
+            if (isset($response['sizes'][ $cur_size ])) {
+                $response['sizes']['medium'] = $response['sizes'][ $cur_size ];
+            }
         }
-		}
-}
+    }
     return $response;
 }
 
-add_filter( 'wp_prepare_attachment_for_js', 'ajust_media_library', 999, 3 );
+add_filter('wp_prepare_attachment_for_js', 'ajust_media_library', 999, 3);
 
 // Register the new image sizes for use in the add media modal in wp-admin
-function foundationpress_custom_sizes( $sizes ) {
-	// Unset WP Default sizes so they can be reordered with new custom sizes
-	unset( $sizes['thumbnail'] );
-	unset( $sizes['medium'] );
-	unset( $sizes['medium_large'] );
-	unset( $sizes['large'] );
-	unset( $sizes['full'] );
+function foundationpress_custom_sizes($sizes)
+{
+    // Unset WP Default sizes so they can be reordered with new custom sizes
+    unset($sizes['thumbnail']);
+    unset($sizes['medium']);
+    unset($sizes['medium_large']);
+    unset($sizes['large']);
+    unset($sizes['full']);
 
-	return array_merge(
-		$sizes,
-		array(
-            'xsmall'       => __( 'XSmall' ),
-            'medium_large' => __( 'MediumLarge' ),
-            'medium'       => __( 'Medium' ),
-            'small'        => __( 'Small' ),
-            'large'        => __( 'Large' ),
-            'xlarge'       => __( 'XLarge' ),
-            'xxlarge'      => __( 'XXLarge'),
-            'full'         => __( 'Full'),
-		)
-	);
+    return array_merge(
+        $sizes,
+        array(
+            'xsmall'       =>  'XSmall',
+            'medium_large' => 'MediumLarge',
+            'medium'       => 'Medium',
+            'small'        => 'Small',
+            'large'        => 'Large',
+            'xlarge'       => 'XLarge',
+            'xxlarge'      => 'XXLarge',
+            'full'         => 'Full',
+        )
+    );
 }
-add_filter( 'image_size_names_choose', 'foundationpress_custom_sizes' );
+add_filter('image_size_names_choose', 'foundationpress_custom_sizes');
 
 // Add custom image sizes attribute to enhance responsive image functionality for content images
-function foundationpress_adjust_image_sizes_attr( $sizes, $size ) {
-	// Actual width of image
-	$width = $size[0];
+function foundationpress_adjust_image_sizes_attr($sizes, $size)
+{
+    // Actual width of image
+    $width = $size[0];
 
-	// Full width page template
-	if ( is_page_template( 'page-templates/page-full-width.php' ) ) {
-		if ( 1200 < $width ) {
-			$sizes = '(max-width: 1199px) 98vw, 1200px';
-		} else {
-			$sizes = '(max-width: 1199px) 98vw, ' . $width . 'px';
-		}
-	} else { // Default 3/4 column post/page layout
-		if ( 770 < $width ) {
-			$sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, 770px';
-		} else {
-			$sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, ' . $width . 'px';
-		}
-	}
+    // Full width page template
+    if (is_page_template('page-templates/page-full-width.php')) {
+        if (1200 < $width) {
+            $sizes = '(max-width: 1199px) 98vw, 1200px';
+        } else {
+            $sizes = '(max-width: 1199px) 98vw, ' . $width . 'px';
+        }
+    } else { // Default 3/4 column post/page layout
+        if (770 < $width) {
+            $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, 770px';
+        } else {
+            $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, ' . $width . 'px';
+        }
+    }
 
-	return $sizes;
+    return $sizes;
 }
-add_filter( 'wp_calculate_image_sizes', 'foundationpress_adjust_image_sizes_attr', 10, 2 );
+add_filter('wp_calculate_image_sizes', 'foundationpress_adjust_image_sizes_attr', 10, 2);
 
 // Remove inline width and height attributes for post thumbnails
-function remove_thumbnail_dimensions( $html, $post_id, $post_image_id ) {
-	if ( ! strpos( $html, 'attachment-shop_single' ) ) {
-		$html = preg_replace( '/^(width|height)=\"\d*\"\s/', '', $html );
-	}
-	return $html;
+function remove_thumbnail_dimensions($html, $post_id, $post_image_id)
+{
+    if (! strpos($html, 'attachment-shop_single')) {
+        $html = preg_replace('/^(width|height)=\"\d*\"\s/', '', $html);
+    }
+    return $html;
 }
-add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10, 3 );
+add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10, 3);
